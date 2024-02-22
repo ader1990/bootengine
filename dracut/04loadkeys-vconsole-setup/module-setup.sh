c8ba5e6c780ccf9e94fcca4aed0721fd994ddcce
Adrian Vladu <avladu@cloudbasesolutions.com>
2024-02-22 11:08:02 +0000

    add loadkeys binary required by systemd-vconsole-setup

--- dracut/04loadkeys-vconsole-setup/module-setup.sh
+++ dracut/04loadkeys-vconsole-setup/module-setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
-# ex: ts=8 sw=4 sts=4 et filetype=sh
+
+check() {
+    require_binaries loadkeys || return 1
+
+    return 0
+}
 
 depends() {
-    echo systemd
+    echo 0
 }
 
 install() {
