ec52cb0c067d9ed8c57acf5909219789fa06ad36
Adrian Vladu <avladu@cloudbasesolutions.com>
2024-02-29 08:27:19 +0000

    add only the keymaps required

--- dracut/04loadkeys-vconsole-setup/module-setup.sh
+++ dracut/04loadkeys-vconsole-setup/module-setup.sh
@@ -12,4 +12,11 @@ depends() {
 
 install() {
     inst_multiple loadkeys || return 1
+
+    # install only the minimum bits
+    INST_DIR_KBD="/usr/share"
+    inst_opt_decompress 
+    inst_dir "$INST_DIR_KBD/keymaps/include"
+    inst_dir "$INST_DIR_KBD/keymaps/i386/include"
+    inst_opt "$INST_DIR_KBD/keymaps/i386/qwerty/us.map.gz"
 }
