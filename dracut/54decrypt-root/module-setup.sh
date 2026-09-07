d9f113d3d71f3034972e813b7424f442eab553d3
Adrian Vladu <avladu@cloudbasesolutions.com>
2026-09-07 09:15:16 +0000

    dracut: decrypt-root: prepare the boilerplate for making the logic more flexible

--- dracut/54decrypt-root/module-setup.sh
+++ dracut/54decrypt-root/module-setup.sh
@@ -1,8 +1,12 @@
 install() {
     inst_rules "$moddir/64-decrypt-root.rules"
 
+    inst_script "$moddir/decrypt-root" \
+        "/usr/sbin/decrypt-root"
+
     inst_simple "$moddir/decrypt-root.service" \
         "$systemdsystemunitdir/decrypt-root.service"
 
     inst_simple /usr/lib64/cryptsetup/libcryptsetup-token-systemd-tpm2.so
+
 }
